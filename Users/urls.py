df8048077f1c6e460e7cecae3ad643a4d305ae93
Mario <marinaldo12@hotmail.com>
2025-10-30 14:36:29 -0300

    rota padrão estabelecida

--- Users/urls.py
+++ Users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserCreateAPIView
 
 urlpatterns = [
-    path("user/cadastro/", UserCreateAPIView.as_view(), name='user_create')
+    path("cadastro/", UserCreateAPIView.as_view(), name='user_create')
 ]
