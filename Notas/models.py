58c3bbf13a9a1da3f83fb0a7234fa3c2aa3955aa
Mario <marinaldo12@hotmail.com>
2025-10-30 14:32:49 -0300

    dono add em notas

--- Notas/models.py
+++ Notas/models.py
@@ -11,6 +11,7 @@ class Notas(models.Model):
 
     dono = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200, null=False, blank=False)
+    descricao = models.TextField( null=True, blank=True, help_text="descrição da anotação")
     data_criacao = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=StatusNotas.choices, default=StatusNotas.PENDENTE)
 
