7dad63d88ba0a4bf4b053fa6a420a3f563b9c055
Mario <marinaldo12@hotmail.com>
2025-10-30 14:34:20 -0300

    modificação de campo email para unique

--- Users/models.py
+++ Users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     #nascimento = models.DateField(null=False, blank=False, help_text="Data de nascimento.")   
-    
+    eamil = models.EmailField(unique=True)
+    #avatar = models.ImageField(upload_to='foto-user', blank=True, null=True)
     def __str__(self):
         return f"O usúario {self.username} cadastrado com sucesso"
