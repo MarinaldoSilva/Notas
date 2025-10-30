3480f81127776cf5255072df3e878a4da8bcf054
Mario <marinaldo12@hotmail.com>
2025-10-30 14:37:35 -0300

    arquivos de configutrações

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# from django.conf import settings
+# from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/cadastrar/', include('Users.urls')),
-    path('api/v1/', include('Notas.urls'))
+    path('api/v1/user/', include('Users.urls')),
+    path('api/v1/', include('Notas.urls')),
+   # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+   # path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
