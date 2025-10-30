00ff8046862840ea0b000e318d338e02a8eb2d37
Mario <marinaldo12@hotmail.com>
2025-10-30 14:33:22 -0300

    validações de notas finalizada

--- Notas/serializer.py
+++ Notas/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Notas
-
+from datetime import datetime
 
 class NotasSerializer(serializers.ModelSerializer):
     dono = serializers.ReadOnlyField(source='dono.username')
@@ -8,3 +8,25 @@ class NotasSerializer(serializers.ModelSerializer):
         model = Notas
         fields = '__all__'
         read_only_fields = ['id','dono']
+
+    def validate_titulo(self, value):
+        min_palavras = 3
+        if len(value.split()) < min_palavras:
+            raise serializers.ValidationError(f"O titulo deve ter pelo menos {min_palavras} palavras")
+        return value.title()
+    
+    def validate(self, data):
+        titulo = data['titulo']
+        descricao = data['descricao']
+
+        if titulo and descricao:
+            if titulo.strip().lower() == descricao.strip().lower():
+                raise serializers.ValidationError("O titulo não pode ser igual a descrição da atividade.")
+            return data
+        
+    def update(self, instance, validated_data):
+        if 'titulo' in validated_data and instance.titulo != validated_data['titulo']:
+            data_update_title = datetime.now().strftime("%d/%m/%y")
+            validated_data['titulo'] = f"{validated_data['titulo']} - Atualizado em {data_update_title}"
+            return super().update(instance, validated_data)
+        return instance
