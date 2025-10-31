c446c559810022b6edf2616aaa10d1a4a2890f20
Mario <marinaldo12@hotmail.com>
2025-10-31 13:17:30 -0300

    configuração de rotas para login, refresh e blacklist do token

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,TokenBlacklistView)
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
@@ -8,9 +8,11 @@ urlpatterns = [
     path('api/v1/user/', include('Users.urls')),
     path('api/v1/', include('Notas.urls')),
 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
