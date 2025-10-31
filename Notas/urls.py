8c4e5c22b10cb987dbd46fe4067266d1209282ab
Mario <marinaldo12@hotmail.com>
2025-10-31 15:43:57 -0300

    alteração das rotas para padronizar a entrada

--- Notas/urls.py
+++ Notas/urls.py
@@ -3,9 +3,9 @@ from .views import (NotasListAPIView, NotasDetailAPIView,
                     NotasCreateAPIView, NotasUpdateAPIView, NotasDestroyAPIView)
 
 urlpatterns = [
-    path('notas/listar/', NotasListAPIView.as_view(), name='listar_nota'),
-    path('notas/listar/<int:pk>/', NotasDetailAPIView.as_view(), name='listar_nota_pk'),
-    path('notas/criar/', NotasCreateAPIView.as_view(), name='criar_nota'),
-    path('notas/editar/<int:pk>/', NotasUpdateAPIView.as_view(), name='update_nota'),
-    path('notas/deletar/<int:pk>/', NotasDestroyAPIView.as_view(), name='deletar_nota')
+    path('criar/', NotasCreateAPIView.as_view(), name='criar_nota'),
+    path('listar/', NotasListAPIView.as_view(), name='listar_nota'),
+    path('listar/<int:pk>/', NotasDetailAPIView.as_view(), name='listar_nota_pk'),
+    path('editar/<int:pk>/', NotasUpdateAPIView.as_view(), name='update_nota'),
+    path('deletar/<int:pk>/', NotasDestroyAPIView.as_view(), name='deletar_nota')
 ]
