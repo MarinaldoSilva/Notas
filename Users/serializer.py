4ec05f4d85f7c891353abda47a56189832447762
Mario <marinaldo12@hotmail.com>
2025-10-31 13:15:10 -0300

    documenteção do swagger feita e correção do campo email

--- Users/serializer.py
+++ Users/serializer.py
@@ -11,7 +11,9 @@ class UserSerializer(serializers.ModelSerializer):
 
         extra_kwargs = {
             'password':{
-                'write_only': True
+                'write_only': True,
+                'min_length':8,
+                'help_text':'A senha deve ter pelo menos 8 digitos'
             }
         }
 
