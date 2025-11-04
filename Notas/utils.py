91f71c5ddc2904d7bec778b465722732181a5f1b
Mario <marinaldo12@hotmail.com>
2025-11-03 23:19:57 -0300

    função de enviar email

--- Notas/utils.py
+++ Notas/utils.py
@@ -2,14 +2,14 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
-def form_email(user_email:str, note_title:str):
-    subject = f"Nova nota '${note_title}' cadastrada com sucesso!"
+def form_email(user_email:str, titulo_nota:str):
+    subject = f"Nova nota '${titulo_nota}' cadastrada com sucesso!"
 
     html_message = render_to_string(
-        'email/new_note_notification.html',
-        {'user_email': user_email, 'note_title': note_title}
+        'email/form_email.html',
+        {'user_email': user_email, 'titulo_nota': titulo_nota}
     )
-    plain_message = f"A nota ${note_title} foi criada"
+    plain_message = f"A nota ${titulo_nota} foi criada"
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [user_email]
 
