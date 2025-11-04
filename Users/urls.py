5e90083636bf0b308bcd7f47af957c273dadc64e
Mario <marinaldo12@hotmail.com>
2025-11-04 12:32:09 -0300

    alteração para o contexto das rotas

--- Users/urls.py
+++ Users/urls.py
@@ -3,5 +3,6 @@ from .views import UserListAPIView, UserUpdateAPIView
 
 urlpatterns = [
     path("listar/", UserListAPIView.as_view(), name='user_list'),
-    path("editar/", UserUpdateAPIView.as_view(), name='editar_user')
+    path("editar/", UserUpdateAPIView.as_view(), name='editar_user'),
+    path("editar/<int:pk>/", UserUpdateAPIView.as_view(), name='editar_user_id')
 ]
