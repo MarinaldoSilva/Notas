10400322541644cdce0179b687cb7d3ac8dbe668
Mario <marinaldo12@hotmail.com>
2025-11-04 12:32:24 -0300

    método patch para user criado

--- Users/views.py
+++ Users/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import User
 from rest_framework import status
 from .serializer import UserSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -21,3 +22,16 @@ class UserUpdateAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def patch(self, request, pk):
+        try:
+            queryset = User.objects.get(pk=pk)
+            if request.user.id != queryset.id:
+                return Response({"errors":"você não term permissão para alterar esse usuário"})
+        except User.DoesNotExist:
+            return Response({"error":"Usuário não localizado"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = UserSerializer(instance=queryset, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
