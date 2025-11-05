c12e1097bec346b2423fc074d5bf62f05893446f
Mario <marinaldo12@hotmail.com>
2025-11-05 16:37:09 -0300

    feat: Remove Users e Notas apps

--- Users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import User
-
-
-@admin.register(User)
-class AdminRegister(admin.ModelAdmin):
-    list_display = ["username", "first_name", "email"]
-    search_fields = ["username", "first_name", "email"]
--- Users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import UserListAPIView, UserUpdateAPIView
-
-urlpatterns = [
-    path("listar/", UserListAPIView.as_view(), name="user_list"),
-    path("editar/", UserUpdateAPIView.as_view(), name="editar_user"),
-    path("editar/<int:pk>/", UserUpdateAPIView.as_view(), name="editar_user_id"),
-]
