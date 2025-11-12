4faa5795a595f38d08ce182555319ba2520698ae
Marinaldo Silva <97764197+MarinaldoSilva@users.noreply.github.com>
2025-11-12 16:36:26 -0300

    Update tasks.py

--- notas/tasks.py
+++ notas/tasks.py
@@ -4,7 +4,6 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 
-# @shared_task vai transformar a função em uma tarefa para o Celery
 @shared_task
 def form_email(email_destinatario: str, titulo_nota: str):
     subject = "Nova nota cadastrada!"
